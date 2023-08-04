0f54bb59c007a9bcce2b7fcac7824ad07f20c295
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-04 23:57:48 +0600

    card design

--- app/page.jsx
+++ app/page.jsx
@@ -8,11 +8,12 @@ async function getData() {
 
 const page = async () => {
   const data = await getData();
+  console.log(data);
 
   return (
     <div className=" w-full h-screen bg-green-100">
       <div className=" container mx-auto">
-        <ul>
+        <ul className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {data.map((user) => {
             return <User key={user.id} user={user} />;
           })}

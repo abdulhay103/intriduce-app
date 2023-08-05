c14811dad54e05689a8dd03ae5b11deb717f053e
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-05 11:45:50 +0600

    working

--- app/src/utils/Header.jsx
+++ app/src/utils/Header.jsx
@@ -1,7 +1,11 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import navData from "../data/navData.js";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
   return (
     <header className="bg-white shadow fixed w-full">
       <nav className=" w-full flex justify-between py-3 container mx-auto">
@@ -9,18 +13,19 @@ const Header = () => {
           <Link href="/">Next User</Link>
         </div>
         <ul className="flex gap-5">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
+          {navData.map((link) => {
+            const isActive = pathname === link.href;
+            return (
+              <li key={link.id}>
+                <Link
+                  className={isActive ? "text-orange-600" : "text-lime-600"}
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>

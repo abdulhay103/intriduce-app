a662ed1dc2461b7b50bdeb7948b6d525d9080245
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-05 11:08:33 +0600

    Header setup done

--- app/page.jsx
+++ app/page.jsx
@@ -8,17 +8,14 @@ async function getData() {
 
 const page = async () => {
   const data = await getData();
-  console.log(data);
 
   return (
-    <div className=" bg-gradient-to-br from-green-50 via-orange-50 to-sky-100 w-full h-screen">
-      <div className=" container mx-auto">
-        <ul className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 md:p-0 lg:p-0">
-          {data.map((user) => {
-            return <User key={user.id} user={user} />;
-          })}
-        </ul>
-      </div>
+    <div className=" container mx-auto py-20">
+      <ul className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 md:p-0 lg:p-0">
+        {data.map((user) => {
+          return <User key={user.id} user={user} />;
+        })}
+      </ul>
     </div>
   );
 };
--- app/src/utils/Header.jsx
+++ app/src/utils/Header.jsx
@@ -1,17 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <ul className=" flex gap-5">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-      </ul>
-    </div>
+    <header className="bg-white shadow fixed w-full">
+      <nav className=" w-full flex justify-between py-3 container mx-auto">
+        <div className=" w-28">
+          <Link href="/">Next User</Link>
+        </div>
+        <ul className="flex gap-5">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 

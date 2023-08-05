e9ea850ac66f4c8855887aee90c0afd0f495bdd5
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-05 13:15:31 +0600

    scrollbar design done

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className=" scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-white">
         <div className=" bg-gradient-to-br from-green-50 via-orange-50 to-sky-100 w-full min-h-screen">
           <Header />
           <div className=" container mx-auto py-20"> {children}</div>

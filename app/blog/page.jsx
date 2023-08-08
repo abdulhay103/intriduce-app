2a1de62c08f4b1b6fc38a8d97f86fa60136c9e1e
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-09 00:38:03 +0600

    cokie setup

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 const page = () => {
+  useEffect(() => {
+    function setCookie(cookieName, cookieValue, expirationDays) {
+      const expirationDate = new Date();
+      expirationDate.setTime(
+        expirationDate.getTime() + expirationDays * 24 * 60 * 60 * 1000
+      );
+
+      const expires = "expires=" + expirationDate.toUTCString();
+      document.cookie =
+        cookieName + "=" + cookieValue + "; " + expires + "; path=/";
+    }
+    setCookie("myCookie", "Hello, Cookie!", 3);
+  }, []);
+
   return (
     <div className="">
       <h1>from Blog page</h1>

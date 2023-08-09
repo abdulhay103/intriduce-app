f38824071909b17b5e97f1e34b49160f09c1a3af
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-09 10:11:50 +0600

    fixed

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,28 +1,28 @@
-// "use client";
-// import { useEffect, useState } from "react";
-// import User from "./userCard/user";
-// import Loader from "../src/components/Loader";
+"use client";
+import { useEffect, useState } from "react";
+import User from "./userCard/user";
+import Loader from "../src/components/Loader";
 
 const page = () => {
-  // const [users, setUsers] = useState();
-  // useEffect(() => {
-  //   (async () => {
-  //     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  //     console.log(res);
-  //     if (res.status == 200) {
-  //       const data = await res.json();
-  //       console.log(data);
-  //       setUsers(data);
-  //     }
-  //   })();
-  // }, []);
-  // console.log(users);
+  const [users, setUsers] = useState();
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      console.log(res);
+      if (res.status == 200) {
+        const data = await res.json();
+        console.log(data);
+        setUsers(data);
+      }
+    })();
+  }, []);
+  console.log(users);
   return (
     <div className="">
       <h1 className="py-3 text-lg font-bold text-orange-500">
         Client side API Rendaring
       </h1>
-      {/* <ul className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 md:p-0 lg:p-0">
+      <ul className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 md:p-0 lg:p-0">
         {users ? (
           users.map((user) => {
             return <User key={user.id} user={user} />;
@@ -30,7 +30,7 @@ const page = () => {
         ) : (
           <Loader />
         )}
-      </ul> */}
+      </ul>
     </div>
   );
 };

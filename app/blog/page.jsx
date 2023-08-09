e4878ddf927444c6eb2681768ba4a63fd7cb709a
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-09 09:50:26 +0600

    set Cokie Remove

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,20 +1,19 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 
 const page = () => {
-  useEffect(() => {
-    function setCookie(cookieName, cookieValue, expirationDays) {
-      const expirationDate = new Date();
-      expirationDate.setTime(
-        expirationDate.getTime() + expirationDays * 24 * 60 * 60 * 1000
-      );
+  // useEffect(() => {
+  //   function setCookie(cookieName, cookieValue, expirationDays) {
+  //     const expirationDate = new Date();
+  //     expirationDate.setTime(
+  //       expirationDate.getTime() + expirationDays * 24 * 60 * 60 * 1000
+  //     );
 
-      const expires = "expires=" + expirationDate.toUTCString();
-      document.cookie =
-        cookieName + "=" + cookieValue + "; " + expires + "; path=/";
-    }
-    setCookie("myCookie", "Hello, Cookie!", 3);
-  }, []);
+  //     const expires = "expires=" + expirationDate.toUTCString();
+  //     document.cookie =
+  //       cookieName + "=" + cookieValue + "; " + expires + "; path=/";
+  //   }
+  //   setCookie("myCookie", "Hello, Cookie!", 3);
+  // }, []);
 
   return (
     <div className="">
